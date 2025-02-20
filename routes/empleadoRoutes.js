af71fbc88a22732f818c206cdd008880f16865b8
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2025-02-20 09:39:24 -0500

    organizacion del post en empleados

--- routes/empleadoRoutes.js
+++ routes/empleadoRoutes.js
@@ -1,4 +1,3 @@
-// empleadoRoutes.js
 import { Router } from 'express';
 import { EmployeesController } from '../controllers/empleadosController.js';  // Asegúrate de que la ruta sea correcta
 import { validateEmployeeId } from '../middlewares/validateEmpleado.js';  // Asegúrate de que la ruta sea correcta
@@ -9,8 +8,10 @@ const router = Router();
 import employeeService from '../services/empleadoService.js';  // Asegúrate de que la ruta sea correcta
 const employeesController = new EmployeesController({ employeeService });
 
-// Definir las rutas y usar los métodos del controlador
-router.get('/', employeesController.get);  // Método `get` del controlador
-router.get('/:id', validateEmployeeId, employeesController.getById);  // Método `getById` del controlador
+router.get('/', employeesController.get);  // Método `get` del controlador (todos los empleados)
+router.get('/:id', validateEmployeeId, employeesController.getById); // Método `getById` del controlador (un empleado por ID)
+router.post('/', employeesController.post);  // Método `post` del controlador (crear empleado)
+router.put('/:id', validateEmployeeId, employeesController.put);  // Método `put` del controlador (actualizar empleado)
+router.delete('/:id', validateEmployeeId, employeesController.delete);  // Método `delete` del controlador (eliminar empleado)
 
 export default router;
