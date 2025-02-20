3988fe48eab5a731e1c22a0b1c0583d0fe8a012e
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2025-02-20 06:20:51 -0500

    funcionando api empleados

--- middlewares/validateEmpleado.js
+++ middlewares/validateEmpleado.js
@@ -1,12 +1,8 @@
-const validateEmployeeId = (req, res, next) => {
-    const { id } = req.params;
-    if (!id || isNaN(id)) {
-      return res.status(400).json({ message: 'Invalid employee ID' });
-    }
-    next();
-  };
-  
-  export default {
-    validateEmployeeId,
-  };
-  
+// validateEmpleado.js
+export const validateEmployeeId = (req, res, next) => {
+  const { id } = req.params;
+  if (!id || isNaN(id)) {
+    return res.status(400).json({ message: 'Invalid employee ID' });
+  }
+  next();
+};
